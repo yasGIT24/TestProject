c44c73bc455f62614449fd77a0f93c26a1e16247
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-11-24 16:03:32 +0530

    Uploading Program.cs

--- Program.cs
+++ Program.cs
@@ -1,45 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using ErrorCodes;
-using ErrorCodeClassLib;
-using MyApp.Errors;
 Console.WriteLine("Hello, World!");
+// Set the directory you want to read from
+//string sourceDirectory = @"C:\Users\yasmin.shahul.hameed\source\repos\GenWizard3.0\GenWizardAgent\2010-GenwizardAgent.DataAccess\Models";
+
+//// Set the path to save the output text file
+//string outputFilePath = @"C:\Users\yasmin.shahul.hameed\Desktop\My Work\Files From Directory\2010-GenwizardAgent.DataAccess-Models.txt";
+
+//try
+//{
+//    // Get all file names in the directory
+//    string[] files = Directory.GetFiles(sourceDirectory);
+//    string[] fileNames = Directory.GetFiles(sourceDirectory)
+//                              .Select(Path.GetFileName)
+//                              .ToArray();
 
 
+//    // Write file names to the text file
+//    File.WriteAllLines(outputFilePath, fileNames);
 
-List<ApplicationErrorMaster> errors = new()
+//    Console.WriteLine("File names written successfully to " + outputFilePath);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("An error occurred: " + ex.Message);
+//}
+
+
+//string key = "mykey";
+//string val1 = "CR Title";
+//string val2 = "CR Desc";
+
+//string result = $"@key:{key} @value:Title as '\\''<{val1}>'\\'' Description as '\\''<{val2}>'\\''";
+
+//Console.WriteLine(result);
+
+
+//Reference check
+Employee emp = new Employee()
 {
-    new ApplicationErrorMaster()
-    {
-        ErrorCode = "Error001",
-        ErrorMessage = "ErrorMessageFor001",
-        UseCase = "UseCase001"
-    },
-     new ApplicationErrorMaster()
-    {
-        ErrorCode = "Error002",
-        ErrorMessage = "ErrorMessageFor002",
-        UseCase = "UseCase002"
-    },
-     new ApplicationErrorMaster()
-    {
-        ErrorCode = "Error003",
-        ErrorMessage = "ErrorMessageFor003",
-        UseCase = "UseCase003"
-    },
-     new ApplicationErrorMaster()
-    {
-        ErrorCode = "Error004",
-        ErrorMessage = "ErrorMessageFor004",
-        UseCase = "UseCase004"
-    },
-      new ApplicationErrorMaster()
-    {
-        ErrorCode = "Error005",
-        ErrorMessage = "ErrorMessageFor005",
-        UseCase = "UseCase005"
-    }
+    Id = 1,
+    Name = "Yasmin Begam",
+    FirstName = "Yasmin",
+    LastName = "Begam",
+    Title = "title",
+    Description = "description",
+    Hobbies = {"Reading", "Writing","Carrom"}
 };
-ErrorRegistry.RegisterErrors(errors);
 
-ErrorCodes.Error error = ErrorRegistry.GetError(ErrorCodeRef.Error005);
-Console.WriteLine(error.ErrorCode +"-"+error.ErrorMessage+"-"+error.UseCase);
+Employee emp2 = emp;
+Employee emp3 = new Employee()
+{
+    Id = 2,
+    Name = "name",
+    FirstName = "fname",
+    LastName = "lname",
+    Title = "title",
+};
+emp3.Hobbies = emp.Hobbies;
+Console.WriteLine("Employee 3 details "+ emp3.Hobbies[0] +"," + emp3.Hobbies[1]);
+emp2.Hobbies.Clear();
+Console.WriteLine("Employee 2 details "+ emp2.Hobbies.Count());
+Console.WriteLine("Employee details " + emp.Hobbies.Count());
+Console.WriteLine("Employee 3 details " + emp3.Hobbies[0] + "," + emp3.Hobbies[1]);
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Title { get; set; }
+    public List<string?> Hobbies { get; set; } = new List<string?>();
+}
+
