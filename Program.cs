f7564fca479a0fe48fad0d3bf332aaa7da0f1464
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-11-24 16:03:30 +0530

    Uploading Program.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ErrorCodes;
+using ErrorCodeClassLib;
+using MyApp.Errors;
+Console.WriteLine("Hello, World!");
+
+
+
+List<ApplicationErrorMaster> errors = new()
+{
+    new ApplicationErrorMaster()
+    {
+        ErrorCode = "Error001",
+        ErrorMessage = "ErrorMessageFor001",
+        UseCase = "UseCase001"
+    },
+     new ApplicationErrorMaster()
+    {
+        ErrorCode = "Error002",
+        ErrorMessage = "ErrorMessageFor002",
+        UseCase = "UseCase002"
+    },
+     new ApplicationErrorMaster()
+    {
+        ErrorCode = "Error003",
+        ErrorMessage = "ErrorMessageFor003",
+        UseCase = "UseCase003"
+    },
+     new ApplicationErrorMaster()
+    {
+        ErrorCode = "Error004",
+        ErrorMessage = "ErrorMessageFor004",
+        UseCase = "UseCase004"
+    },
+      new ApplicationErrorMaster()
+    {
+        ErrorCode = "Error005",
+        ErrorMessage = "ErrorMessageFor005",
+        UseCase = "UseCase005"
+    }
+};
+ErrorRegistry.RegisterErrors(errors);
+
+ErrorCodes.Error error = ErrorRegistry.GetError(ErrorCodeRef.Error005);
+Console.WriteLine(error.ErrorCode +"-"+error.ErrorMessage+"-"+error.UseCase);
