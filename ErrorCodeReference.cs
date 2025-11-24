275732aaf335b9cf8d324adea7bab97e4b1b099f
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-11-24 16:03:22 +0530

    Uploading ErrorCodeReference.cs

---
+++ ErrorCodeReference.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MyApp.Errors
+{
+    public static class ErrorCodeRefs
+    {
+        public const string InvalidInput = "Error001";
+        public const string FileTooLarge = "Error002";
+        public const string MissingField = "Error003";
+    }
+}
