14672a42adbf2a07ba45097f2afdeb90e4ad03c3
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-11-24 16:03:24 +0530

    Uploading ErrorCodeRef.cs

---
+++ ErrorCodeRef.cs
@@ -0,0 +1,12 @@
+﻿namespace ErrorCodes
+{
+    public static class ErrorCodeRef{
+         public const string Error001 = "Error001";
+          public const string Error002 = "Error002";
+          public const string Error003 = "Error003";
+          public const string Error004 = "Error004";
+          public const string Error005 = "Error005";
+         }
+    }
+
+    
